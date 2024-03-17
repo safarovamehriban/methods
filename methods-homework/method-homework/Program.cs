618954ea643982c5895f9ace14a61bc40d7e950c
safarovamehriban <smh04@mail.ru>
2024-03-18 01:48:15 +0400

    methods added

---
+++ methods-homework/method-homework/Program.cs
@@ -0,0 +1,123 @@
+﻿namespace method_homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            //    //1 ci task:  2 string array-i içerisindeki ortaq elementleri tapib ekrana yazdiran metod;
+
+            //    string[] arr = { "1", "12", "3", "14", "5", "6", "7", "18" };
+            //    string[] arr1 = { "11", "12", "13", "14", "5", "16", "17", "18" };
+
+            //    CommonString(arr, arr1);
+            //}
+
+            //static void CommonString(string[] arr, string[] arr1)
+            //{
+            //    var commonElements = arr.Intersect(arr1);
+
+            //    if (commonElements.Any())
+            //    {
+            //        Console.WriteLine("Ortaq String");
+
+            //        foreach (var element in commonElements)
+            //        {
+            //            Console.WriteLine(element);
+            //        }
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Ortaq string tapilmadi");
+            //    }
+            //        }
+            //    }
+            //}
+
+
+
+
+            //2 ci task: 2 eyni uzunluqlu array içerisindeki elementlerin indexlerine uygun hasilini tapib ekrana yazdiran metod
+            //Muellim bunu yaza bilmedim((
+
+
+
+            //            //3 cu task: Array -in içerisinde verilmiş herf ile başlayan elementleri ekrana yazdıran method 
+
+            //            string[] arr = { "Ruslan", "Arsen", "Rasul", "Elvin", "Rizvan", "Arif" };
+
+            //            for (int i = 0; i < arr.Length; i++)
+            //            {
+            //                for (int j = 0; j < arr[i].Length; j++)
+            //                {
+
+            //                    if (arr[i][j] == 'a')
+            //                    {
+            //                        Console.WriteLine(arr[i]);
+            //                        break;
+            //                    }
+            //                }
+
+            //            }
+            //        }
+            //    }
+            //}
+
+
+//            // 4.Area deyə Method(lar) yaradın
+
+//            int a = 15, b = 25, c = 30, r = 10, p = 3;
+//            CircleArea(r,p);
+//            RectangleArea(a,b);
+//            TotalSurfaceArea(a, b, c);
+//            InscribedCircleArea(p, r, a, b, c);
+
+//        }
+
+
+//        //a.Çevrənin sahəsi -S = p * r² (p = 3)
+//        static int CircleArea(int r, int p)
+//        {
+//            int circlearea = p * r * r(p = 3);
+//            Console.WriteLine(CircleArea);
+//            return CircleArea();
+//        }
+
+
+//        //b.Düzbucaqlının sahəsi -S = a * b
+//        static int RectangleArea(int a, int b)
+//        {
+//            int rectanglearea = a * b;
+//            Console.WriteLine(RectangleArea);
+//            return RectangleArea;
+//        }
+
+
+//        //c.Düzbucaqlı paralelpipedin tam səthinin sahəsi - S = 2(a * b + a * c + b * c)
+
+//        static int TotalSurfaceArea(int a, int b, int c)
+//        {
+//            int totalsurfacearea = 2(a * b + a * c + b * c);
+//            Console.WriteLine(totalsurfacearea);
+//            return TotalSurfaceArea;
+//        }
+
+
+//        //d.Üçbucaqlının daxilinə çəkilmiş çevrənin sahəsi - S = p * r; p = (a + b + c) / 2
+
+//        static int InscribedCircleArea(int a, int b, int c)
+//        {
+//            int inscribedcirclearea = p*r;
+//            Console.WriteLine(inscribedcirclearea);
+//            return InscribedCircleArea;
+//        }
+//    }
+
+
+//}
+
+
+
+
